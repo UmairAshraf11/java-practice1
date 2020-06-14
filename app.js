0275abe3ace0283ec98d210cffeb94953368c6e2
UmairAshraf11 <umairashraf569@gmail.com>
2020-06-14 06:13:34 -0700

    java12

---
+++ app.js
@@ -0,0 +1,626 @@
+//CH # 01 TASK 1
+
+//Q no 1  Write a script to greet your website visitor using JS alert box. 
+               //ANSWERS
+
+//var a = "GOOD MORNING"
+ //var newline ="\r\n"
+//var b = "THANKS FOR VISIT OUR WEBSITE  "
+ //var newline ="\r\n"
+//var c = "can ! we help you "
+//var d = a + newline+ b + newline + c 
+//alert(d)
+
+
+
+//Q no 2
+          //ANSWERS    
+//var a =    "JavaScript Alert"
+  //var newline ="\r\n"
+ // var newline1 ="\r\n"
+//var b =  "Error!";
+//var c =   "please enter a valid password";
+//var d = a  +   newline + newline1 + b +c ;
+//alert(d)
+
+
+//Q no 3
+         //ANSWERS    
+    // var a =    "JavaScript Alert"
+    // var newline ="\r\n"
+    // var newline ="\r\n"
+     //var b = "WELCOME TO JS LAND \nHAPPY CODING "
+   //  var c = a + newline + b
+     //alert(c)
+
+// Q NO#4
+//var a = "JavaScript Alert"
+//var newline = "\r\n"
+//var newline = "\r\n"
+//var b = "Welcome To Js Land \n "
+
+//var d = a + newline + newline + b 
+//alert(d)
+
+//var a = "JavaScript Alert"
+//var newline = "\r\n"
+//var newline = "\r\n"
+//var b = "Happy Coding! \n "
+//var c = "prevent this page from creating additional dialog's"
+//var d = a + newline + newline + b +c 
+//alert(d)
+
+
+
+//  Q NO#5
+
+    // var a = prompt("java script alert", "Helo")
+     //var b = a + "! i can run js through my web bbrowser console"
+    // console.log(b)
+   
+// Q NO 6
+
+//var a = prompt("enter name ")
+//var b = "welcome dear"
+//var c= b +" "+ a
+//alert(c)
+
+
+//Q no 7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///  CHAPTER NO 02
+
+// Q NO 1 declared variable
+ // var username = "umair"
+ // alert(username)
+
+
+ // Q NO 2:
+   //var  myName = "Umair Ashraf"
+   //alert(myName)
+
+
+   //Q N0 3
+   //var a = "MESSAGE "
+   //newline ="\r\n"
+   //var b =  "Helo World "
+   //var c = a + newline + b
+   //alert(c)
+
+
+   //Q NO 04
+   //var name = "Umair"
+   //alert(name)
+   //var age = "25 years old"
+   //alert(age)
+   //var Qualification = "Certified Mobile Application Development"
+   
+   //alert(Qualification)
+
+   // Q no 5
+   //var a = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+   //alert(a)
+   
+   
+   //Q no 6
+   
+   //var email = "UmairAshraf" + "569" + "@" + "gmail.com";
+   //alert(email);
+
+
+
+
+   //Qno 7
+   //var book = "A Smarter Way To Learn JavaScript"
+   //alert("I Am Trying To Learn From The Book" + " " + book)
+
+
+   //Q NO 8
+
+   //var a  = "Yah !"
+   //var b = " I  Can Write HTML Content Through Java Script"
+   //document.write(c)
+   // Q NO 9
+   //var Design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ "
+   //alert(Design);
+ 
+
+
+
+
+
+
+   // CHAPTER NO #3
+   // Q NO 1
+//var a = "I am"
+//var age = "18 years old"
+//var b = a + age
+//alert(b)
+
+
+// Q NO 2
+
+
+
+
+
+
+
+
+
+
+
+// Q NO 3
+//var birth = "MY BIRTH YEAR IS"
+//var birthYear = 1996
+//newline = "<br>"
+//var data = "Data type of my declared vaiable is number"
+//var datatype = birth + birthYear + newline + data
+//document.write(datatype)
+
+
+// Q No 4
+ //var name = prompt(" entre your name")
+ //var product = prompt("enter product title")
+ //var  quantity = prompt("total buying quantity of product")
+ //var total = name +"  " + "orderd"+ "  " +quantity +  "    " + product +  "  "  +"on " + "  " + "METRO SHOES"
+ //document.write(total)
+ 
+
+// CH NO 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CHAP NO 5
+
+// Q NO 1
+//var a =3
+//var b = 5
+//var c = a+b
+//document.write("SUM OF 3 AND 5 IS" + " " + c )
+   //Q NO 2  SUBTRACTION
+//var a =3
+//var b = 5
+//var c = a-b
+//document.write("SUBTRACTION OF 3 AND 5 IS" + "    " + c )
+
+// MULTIPLICATION
+//var a =3
+//var b = 5
+//var c = a*b
+//document.write("MULTIPLICATION OF 3 AND 5 IS" + " " + c )
+// DIVISSION
+//var a =3
+//var b = 5
+//var c = a/b
+//document.write("MULTIPLICATION OF 3 AND 5 IS" + " " + c )
+// MODULOUS
+//var a =3
+//var b = 5
+//var c = a%b
+//document.write("MULTIPLICATION OF 3 AND 5 IS" + " " + c )
+
+
+// Q NO 3
+// a
+  //var a = ""
+// b
+//document.write("value after declaration  UNDEFINED <br>" )
+// c 
+//var a = 5
+//d
+//document.write("Initial value " + a  +"<br>")
+// E 
+//var a = ++a
+//document .write("Value after increment "+" "+a+"<br>")
+// g
+//var a = a
+//var b = a +7
+//h
+//document .write("Value after ADDITION "+" "+b +"<br>")
+//i
+//var a = 13
+
+//document.write("Decrement variable"+a+"<br>")
+//j 
+//var a = --a
+//document.write("Value after decrement"+a+"<br>")
+//k
+//var a = a
+//var b = 3
+//var c = a%b
+//document.write("The reminder is "+c)
+
+
+
+
+
+
+
+
+
+
+// Q NO 4
+//var movieticket = "600"
+//var buyingticket ="5" 
+//var totalcost = movieticket*buyingticket
+//document.write("TOTAL COST TO BUY 5 TICKETS TO A MOVIE IS" +"   "+totalcost +"  "+"PKR")
+
+
+
+
+
+
+// Q NO 5
+//document.write("12" + "x" + "1" + "=" + 12*1 + "<br>")
+//document.write("12" + "x" + "2" + "=" + 12*2 + "<br>")
+//document.write("12" + "x" + "3" + "=" + 12*3 + "<br>")
+//document.write("12" + "x" + "4" + "=" + 12*4 + "<br>")
+//document.write("12" + "x" + "5" + "=" + 12*5 + "<br>")
+//document.write("12" + "x" + "6" + "=" + 12*6 + "<br>")
+//document.write("12" + "x" + "7" + "=" + 12*7 + "<br>")
+//document.write("12" + "x" + "8" + "=" + 12*8 + "<br>")
+//document.write("12" + "x" + "9" + "=" + 12*9 + "<br>")
+//document.write("12" + "x" + "10" + "=" + 12*10 + "<br>")
+
+
+// Q NO 7
+
+
+
+//var cart = "SHIPPING CART <br><br><br>"
+//document.write(cart)
+//var priceitem1 = 250
+//document.write("price of item 1 is "+""+priceitem1+"<br>")
+//var priceitem2 =250
+//document.write("price of item 2 is "+""+priceitem2+"<br>")
+//var orderquantityitem1=4
+//document.write("order  quantity   of item 1 is "+""+orderquantityitem1+"<br>")
+//var orderquantityitem2=4
+//document.write("order  quantity   of item 2 is "+""+orderquantityitem2+"<br>")
+//var shipping = 200
+//var b = priceitem1*orderquantityitem1 +priceitem2*orderquantityitem2+shipping
+//document.write("Total cost of your order"+b)
+
+// Q NO 8
+//var total = 950
+//document.write("Toal Marks is "+""+total+"<br>")
+//var obtain = 780
+//document.write("Obtain Marks is "+""+obtain+"<br>")
+//var percentage = obtain/total*100
+//document.write("Percentage"+""+percentage +"%")
+
+
+
+
+//Q NO 9
+//var PAKISTAN="<h1>CURENCY IN PAKISTAN</h1>"
+//document.write(PAKISTAN)
+//var usdollar = 10 
+//var saudiRayal = 25
+//var dollarsinpakistan = usdollar*104.8
+//var saudiRayalinpakistan= saudiRayal*28
+//var PKR =dollarsinpakistan+saudiRayalinpakistan
+//document.write("TOTAL CURENCY IN PAKISTAN "+""+PKR)
+
+
+
+//Q NO 10
+//var a = 10
+//var b = ((a + 5)*10)/2
+//document.write("THE ANSWERS IS"+"   "+b)
+
+
+// Q NO 11
+//var currentyear=prompt("enter current year ")
+
+//document.write("THE CURRENT YEAR IS"+"  "+currentyear+"<br>")
+//var birthyear=prompt("enter Birth year")
+//document.write("THE BIRTH YEAR IS"+"  "+birthyear+"<br>")
+//var age = birthyear-currentyear
+//document.write("you are"+""+age+""+"Years old")
+
+// chapter 6---9
+//Q NO 1
+//var a = 20
+//
+//document.write("RESULT<br> The Value of a is :" + a+"<br>")
+
+//document.write("<br>The value of ++a is :"+ ++a)
+//document.write("<br> Now the value of a is : "+ a+"<br>")
+//document.write("<br> The value of a++ is :"+ a++)
+//document.write("<br> Now the value of a is :"+ a +"<br>")
+//document.write("<br> The value of --a is :"+ --a)
+//document.write("<br> Now the value of a is :"+ a+"<br>")
+//document.write("<br> The value of a-- is : "+ a--)
+//document.write("<br> Now the value of a is: "+ a)
+
+
+//Q NO 02
+//var a= 2
+//var b= 1
+//var c= --a - --b + ++b + b--; 
+//document.write("the value of a is"+ a +"<br>")
+//document.write("the value of --a is ="+ --a +"<br>")
+//document.write("<br> the value of --b is ="+ --b)
+//document.write("<br>the value of --a - --b is  =" +1+ "<br>")
+//document.write("<br> the value of --a - --b + ++b is = "+4)
+//document.write("<br> the value of --a - --b + ++b + b-- ="+3+"<br>")
+//document.write("The Answer is"+c)
+
+
+ //Q no 3
+//var a = prompt("enter name ")
+//var b = "welcome dear"
+//var c= b +" "+ a
+//alert(c)
+// Q NO 6
+//var subject = "subject total marks obtained marks <br><tr><td>english     100  </td><td>   500 </td></tr><br><tr></tr><td>URDU<td><br><tr><td>MATH</td></tr>"
+//document.write(subject)
+
+/// ch 9--10
+// Q no 1
+
+//var b = prompt("Enter city name")
+//if (b==="karachi"){
+ // alert ("welcome to city of lights" )
+//}
+////else{
+  //alert("you entered wrong informaion")
+//}
+//Q NO 2
+
+//var a = prompt("Enter Your Gender")
+//if (a==="male"){
+  //alert ("good morning sir" )
+  
+//}else{
+  //alert("GOOD MORNING MAM")
+//}
+// Q NO4
+//var a = prompt("entre fuel")
+//if (a < "0.25"){
+  //alert("PLEASE REFILL THE FUEL IN YOUR CAR")
+//}
+//else{
+  //alert("FUEL IS MORE THEN FOR YOUR REUIRMENT")
+//} 
+//Q NO 5
+
+//////////AAAAA////
+//var a = 4
+//if (++a===5){
+ // alert("given condition is true") 
+//}
+///////BBBBBBBBBBBBBBBB/////
+//var b = 82
+//if(b++ ===="83"){
+ // alert("given condition is true")
+//}
+
+
+/// ccccc////
+//var c = 12
+ //c= c++
+//if (c++ ===13){
+ //alert("condition 1 is true")
+//}
+  //if(c==13){
+    //alert("condition 2 is true")
+  //}
+  //if(++c<4){
+   // alert("condition 3 is ")
+ // }
+  //if(c===14){
+    //alert("condition 4 is ")
+  //}
+  //////DDDDD///
+  //var materialcost=20000
+  //var labourcost=2000
+  //var total= materialcost+labourcost
+  //
+  //if(totalcost==="labourcost"+"materialcost"){
+    //alert("The cost equal")
+  //}
+  //else{
+    //alert("not ")
+  //}
+
+  /// Q no 8
+  //var a =  prompt("enter value")
+  //if (a%3===0){
+  //  alert(a+"divisible by 3")
+  //}
+  //else{
+  //  alert(a+"number is not divisible by 3")
+  //}
+  
+
+/// Q NO 9
+  //var num = prompt("enter the Number");
+  ////document.write("Number = "+num+"<br>");
+  //if(num % 2 == 0) {
+    // document.write('Number is even!');
+  //} else {
+   //  document.write('Number is odd!');
+  //}
+ /// QQ NO 07
+    
+    //var num = prompt("enter value")
+    //var guess;
+   
+    //while (guess != num) {
+      //guess = prompt('Guess the number between 1 and 20 inclusive');
+    //}
+    //alert('Congratulations!\nThe number was ' + num);
+
+
+
+
+  /// CH 12 --13
+  // Q NO 2
+  //var a = prompt("enter num")
+  //var b= prompt("enter number")
+  //if (a>b){
+   // alert("A is greater")
+    
+  //}
+  //else{
+   // alert("B IS greater")
+  //}
+    
+  
+  /// Q NO 3
+  //var num =prompt("enter number")
+  
+  //if (num ==="0"){
+    
+   // alert("The Num is zero")
+ ////// }
+ // else{
+   // alert("wrong information")
+  //}
+  //var num =prompt("enter number")
+  
+  //if (num <"0"){
+    
+    //alert("The Num is negative")
+  //}
+  //else{
+   // alert("The Num is positive")
+  //}
+    // Q NO 6
+  //var greeting;
+  // var hour = 13;
+   // if (hour < 18) {
+    ////  alert("Good day") 
+     // }
+       // else {
+         // alert("Good evening")
+        //} ; 
+    
+// Q NO 4
+
+
+	// var text;
+	// var argument = argument.toLowerCase();
+
+  //   var vowels = (['a', 'e', 'i', 'o', 'u']);
+
+  //   for (var i = 0; i <= vowels.length; i++){
+  //       if (argument != vowels[i]) {
+  //          alert(vowels+argument)}
+  //         }
+     
+ 
+
+  
+
+
+// chapter 14-16
+ //Q NO 1
+//var a;
+//var a =[]
+
+//
+
+//Q NO 3
+// var a =["umair","Asad","faizan"]
+//console.log(a[1])
+
+
+// Q NO 4
+//numbers array
+//var a = "student name";
+//a = [];
+ // Q NO 5
+ //var number;
+ //var number =[1,2,3,4]
+ // Q NO 6
+ //var array;
+ //var array = ["umair",1,"asad", 6] 
+// Q NO 7
+//var a =[]
+//a.push( "1)SSC"+ "<br>"+ "2)HSC"+"<br>"+ "3)BCS"+"<br>"+"4)BS" +"<br>"+"5)BCOM"+"<br>"+ "6)MS"+"<br>"+ "7)M. Phil"+"<br>"+ "8)PhD")
+//document.write(a)
+/// Q NO 8
+
+
+
+
+
+/// CH 17---20//
+
+
+  
+class table 
+{    
+    // Driver code 
+    public static void main(String arg[]) 
+    {    
+        // Change here to change output 
+        int n = 5;  
+          
+        for (int i = 1; i <= 10; ++i)  
+            System.out.println(n + " * " + i + 
+                               " = " + n * i); 
+    } 
+} 
+  
+// This code is contributed by Anant Agarwal. 
+
+ 
+
+      
